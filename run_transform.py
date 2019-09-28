4e1cbb6e431437de12234fca1f3071a20d0d7084
Harry <harryafirth@gmail.com>
2019-09-28 11:45:26 +0100

    Minor cleanup of transform code

--- run_transform.py
+++ run_transform.py
@@ -1,6 +1,5 @@
 import os
 import json
-import types
 import re
 
 import pandas as pd
@@ -10,9 +9,15 @@ import numpy as np
 DATA_LOC = 'data/'
 
 
-# TODO: CHECK UNIQUE INDEX!
 def dval_unique_index(df):
     return len(df.index.unique()) == len(df.index)
+
+
+def pandas_integerstr_to_int(x):
+    if np.isnan(x):
+        return np.nan
+    else:
+        return re.sub(r'(\.\d+)', '', str(x))
 
 
 # TODO: Automate or get rid of temporary files
@@ -28,21 +33,21 @@ with open(os.path.join(DATA_LOC, f'players_{DT2}.json'), 'r') as f:
 with open(os.path.join(DATA_LOC, f'main_{DT3}.json'), 'r') as f:
     main_data = json.load(f)
 
-######## FIXTURES
-fixtures_cols = {'code': 'fixture_id_long',
-                 'event': 'gameweek',
-                 'finished': 'fixture_finished',
-                 'finished_provisional': 'fixture_finished_provisional',
-                 'id': 'fixture_id',
-                 'minutes': 'fixture_minutes',
-                 'started': 'fixture_started',
-                 'team_a': 'away_team_id',
-                 'team_a_score': 'away_team_score',
-                 'team_h': 'home_team_id',
-                 'team_h_score': 'home_team_score',
-                 'team_h_difficulty': 'home_team_fixture_difficulty',
-                 'team_a_difficulty': 'away_team_fixture_difficulty'
-                 }
+# Data: fixtures
+fixtures_rename = {'code': 'fixture_id_long',
+                   'event': 'gameweek',
+                   'finished': 'fixture_finished',
+                   'finished_provisional': 'fixture_finished_provisional',
+                   'id': 'fixture_id',
+                   'minutes': 'fixture_minutes',
+                   'started': 'fixture_started',
+                   'team_a': 'away_team_id',
+                   'team_a_score': 'away_team_score',
+                   'team_h': 'home_team_id',
+                   'team_h_score': 'home_team_score',
+                   'team_h_difficulty': 'home_team_fixture_difficulty',
+                   'team_a_difficulty': 'away_team_fixture_difficulty'
+                   }
 
 fixtures_drop = ['kickoff_time', 'provisional_start_time']
 fixtures_str_cols = ['fixture_id',
@@ -56,29 +61,25 @@ df_fixtures = pd.DataFrame(fixtures_data)
 df_fixtures['fixture_kickoff_datetime'] =\
     pd.to_datetime(df_fixtures['kickoff_time'], errors='coerce')
 df_fixtures.drop(columns=['stats'] + fixtures_drop, inplace=True)
-df_fixtures.rename(columns=fixtures_cols, inplace=True)
+df_fixtures.rename(columns=fixtures_rename, inplace=True)
 df_fixtures[fixtures_str_cols] = df_fixtures[fixtures_str_cols].astype(str)
-
 df_fixtures.sort_values(fixtures_index, inplace=True)
 
-
-
-
-######### Gameweeks
-gameweek_cols = {'id': 'gameweek',
-                 'name': 'gameweek_name',
-                 'finished': 'gameweek_finished',
-                 'data_checked': 'gameweek_data_checked',
-                 'highest_score': 'highest_scoring_entry_score',
-                 'is_previous': 'gameweek_previous',
-                 'is_current': 'gameweek_current',
-                 'is_next': 'gameweek_next',
-                 'most_selected': 'player_id_most_selected',
-                 'most_transferred_in': 'player_id_most_transferred_in',
-                 'top_element': 'player_id_highest_score',
-                 'most_captained': 'player_id_most_captained',
-                 'most_vice_captained': 'player_id_most_vice_captained'
-                 }
+# Data: gameweeks
+gameweek_rename = {'id': 'gameweek',
+                   'name': 'gameweek_name',
+                   'finished': 'gameweek_finished',
+                   'data_checked': 'gameweek_data_checked',
+                   'highest_score': 'highest_scoring_entry_score',
+                   'is_previous': 'gameweek_previous',
+                   'is_current': 'gameweek_current',
+                   'is_next': 'gameweek_next',
+                   'most_selected': 'player_id_most_selected',
+                   'most_transferred_in': 'player_id_most_transferred_in',
+                   'top_element': 'player_id_highest_score',
+                   'most_captained': 'player_id_most_captained',
+                   'most_vice_captained': 'player_id_most_vice_captained'
+                   }
 gameweek_drop = ['deadline_time',
                  'deadline_time_epoch',
                  'deadline_time_game_offset',
@@ -96,21 +97,13 @@ gameweek_str_cols = ['gameweek',
 df_gameweeks = pd.DataFrame(main_data['events'])
 df_gameweeks['gameweek_deadline_time'] =\
     pd.to_datetime(df_gameweeks['deadline_time'], errors='coerce')
-df_gameweeks.drop(columns=['chip_plays', 'top_element_info'] + gameweek_drop,
-                  inplace=True)
-df_gameweeks.rename(columns=gameweek_cols, inplace=True)
-
-
+df_gameweeks.drop(columns=gameweek_drop, inplace=True)
+df_gameweeks.rename(columns=gameweek_rename, inplace=True)
 df_gameweeks[gameweek_str_cols] = df_gameweeks[gameweek_str_cols]\
-    .applymap(lambda x: re.sub(r'(\.\d+)', '', str(x)) if not np.isnan(x) else np.nan)
-
-
+    .applymap(pandas_integerstr_to_int)
 df_gameweeks.sort_values(gameweek_index, inplace=True)
 
-
-
-
-
+# Data: teams
 teams_rename = {'code': 'team_id_long',
                 'id': 'team_id',
                 'name': 'team_name_long',
@@ -134,7 +127,7 @@ df_teams.drop(columns=teams_drop, inplace=True)
 df_teams[teams_str_cols] = df_teams[teams_str_cols].astype(str)
 df_teams.sort_values(teams_index, inplace=True)
 
-
+# Reference data: positions
 positions_rename = {'id': 'position_id',
                     'singular_name': 'position_name_long',
                     'singular_name_short': 'position_name'}
@@ -149,7 +142,8 @@ df_positions.drop(columns=positions_drop, inplace=True)
 df_positions[positions_str_cols] = df_positions[positions_str_cols].astype(str)
 df_positions.sort_values(positions_index, inplace=True)
 
-
+# Data: players - single row per player with current stats for this point and
+# aggregated up to this point
 players_sum_rename = {'code': 'player_id_long',
                       'element_type': 'position_id',
                       'event_points': 'gameweek_points',
@@ -166,17 +160,12 @@ df_players_sum['news_added_datetime'] = pd.to_datetime(
     errors='coerce')
 df_players_sum.rename(columns=players_sum_rename, inplace=True)
 df_players_sum.drop(columns=players_sum_drop, inplace=True)
-df_players_sum[players_sum_str_cols] = df_players_sum[players_sum_str_cols].astype(str)
+df_players_sum[players_sum_str_cols] =\
+    df_players_sum[players_sum_str_cols].astype(str)
 df_players_sum.sort_values(players_sum_index, inplace=True)
 
-
-df_players_append = df_players_sum.copy()
-
-
-
-player_id_mapping = df_players_sum.copy()[['player_id', 'player_id_long']]
-
-
+# Data: players - one row per player per fixture with stats for that fixture
+# only
 df_players_past = []
 df_players_future = []
 df_players_prev_seasons = []
@@ -187,22 +176,24 @@ for k, p in player_data.items():
     df_players_future.append(fut)
     df_players_prev_seasons.append(pd.DataFrame(p['history_past']))
 
+# Data: player performance in previous seasons
 players_prev_seasons_rename = {'element_code': 'player_id_long'}
 players_prev_seasons_drop = []
 players_prev_seasons_index = ['player_id_long', 'season_name']
 players_prev_seasons_str_cols = ['player_id_long']
 
 df_players_prev_seasons = pd.concat(df_players_prev_seasons)
-df_players_prev_seasons.rename(columns=players_prev_seasons_rename, inplace=True)
+df_players_prev_seasons.rename(columns=players_prev_seasons_rename,
+                               inplace=True)
 df_players_prev_seasons.drop(columns=players_prev_seasons_drop, inplace=True)
-df_players_prev_seasons[players_prev_seasons_str_cols] = df_players_prev_seasons[players_prev_seasons_str_cols].astype(str)
+df_players_prev_seasons[players_prev_seasons_str_cols] =\
+    df_players_prev_seasons[players_prev_seasons_str_cols].astype(str)
 df_players_prev_seasons.sort_values(players_prev_seasons_index, inplace=True)
 
-
-
+# Data: players in previous fixtures this season
 players_past_rename = {'element': 'player_id',
-                                'fixture': 'fixture_id',
-                                'round': 'gameweek',
+                       'fixture': 'fixture_id',
+                       'round': 'gameweek',
                        'was_home': 'fixture_home'}
 players_past_drop = ['kickoff_time', 'opponent_team']
 players_past_str_cols = ['player_id', 'fixture_id', 'gameweek']
@@ -216,60 +207,67 @@ df_players_past['kickoff_datetime'] = pd.to_datetime(
 df_players_past.drop(columns=players_past_drop, inplace=True)
 df_players_past[players_past_str_cols] =\
     df_players_past[players_past_str_cols].astype(str)
+df_players_past = pd.merge(df_players_past, df_fixtures[
+    ['fixture_id', 'fixture_id_long', 'away_team_id', 'home_team_id']],
+                           how='inner',
+                           right_on='fixture_id',
+                           left_on='fixture_id'
+                           )
 df_players_past.sort_values(players_past_index, inplace=True)
 
-
-
+# Data: players' remaining fixtures
 player_future_rename = {'event': 'gameweek',
-                                  'code': 'fixture_id_long',
-                                  'team_h': 'home_team_id',
-                                  'team_a': 'away_team_id',
-                       'is_home': 'fixture_home'}
+                        'code': 'fixture_id_long',
+                        'team_h': 'home_team_id',
+                        'team_a': 'away_team_id',
+                        'is_home': 'fixture_home'}
 players_future_drop = ['kickoff_time', 'event_name']
-players_future_str_cols = ['fixture_id_long', 'gameweek', 'home_team_id', 'away_team_id']
+players_future_str_cols = ['fixture_id_long',
+                           'gameweek',
+                           'home_team_id',
+                           'away_team_id']
 players_future_index = ['player_id', 'fixture_id_long']
 df_players_future = pd.concat(df_players_future)
 df_players_future.rename(columns=player_future_rename, inplace=True)
-df_players_future['kickoff_datetime'] = pd.to_datetime(df_players_future['kickoff_time'],
-                                                     errors='coerce')
+df_players_future['kickoff_datetime'] =\
+    pd.to_datetime(df_players_future['kickoff_time'], errors='coerce')
 df_players_future.drop(columns=players_future_drop, inplace=True)
 df_players_future[players_future_str_cols] =\
     df_players_future[players_future_str_cols].astype(str)
+df_players_future = pd.merge(df_players_future,
+                             df_fixtures[['fixture_id', 'fixture_id_long']],
+                             how='inner',
+                             on='fixture_id_long'
+                             )
 df_players_future.sort_values(players_future_index, inplace=True)
 
-df_players_past2 = pd.merge(df_players_past, df_fixtures[
-    ['fixture_id', 'fixture_id_long', 'away_team_id', 'home_team_id']],
-                            how='inner',
-                            right_on='fixture_id',
-                            left_on='fixture_id'
-                            )
-df_players_future2 = pd.merge(df_players_future, df_fixtures[['fixture_id', 'fixture_id_long']],
-                            how='inner',
-                            on='fixture_id_long'
-                            )
-
-
-
-df_players_full = pd.concat((df_players_past2, df_players_future2), sort=False)
+# Data: players - one row per fixture for this season's previous and remaining
+# fixtures
+df_players_full = pd.concat((df_players_past, df_players_future), sort=False)
 
 df_players_full.sort_values(['player_id', 'kickoff_datetime'], inplace=True)
 df_players_full['team_id'] = np.where(df_players_full['fixture_home'],
                                       df_players_full['home_team_id'],
                                       df_players_full['away_team_id'])
+df_players_full = pd.merge(df_players_full,
+                           df_players_sum[['player_id', 'position_id']],
+                           how='left',
+                           on='player_id')
 
+df_fixtures.set_index(fixtures_index, inplace=True)
+df_gameweeks.set_index(gameweek_index, inplace=True)
+df_teams.set_index(teams_index, inplace=True)
+df_positions.set_index(positions_index, inplace=True)
+df_players_sum.set_index(players_sum_index, inplace=True)
+df_players_prev_seasons.set_index(players_prev_seasons_index, inplace=True)
+df_players_past.set_index(players_past_index, inplace=True)
+df_players_future.set_index(players_future_index, inplace=True)
 
-
-
-# df_fixtures.set_index(fixtures_index, inplace=True)
-# df_gameweeks.set_index(gameweek_index, inplace=True)
-# df_teams.set_index(teams_index, inplace=True)
-# df_positions.set_index(positions_index, inplace=True)
-# df_players_sum.set_index(players_sum_index, inplace=True)
-# df_players_prev_seasons.set_index(players_prev_seasons_index, inplace=True)
-# df_players_past.set_index(players_past_index, inplace=True)
-# df_players_future.set_index(players_future_index, inplace=True)
-#
-# assert dval_unique_index(df_fixtures)
-# assert dval_unique_index(df_gameweeks)
-# assert dval_unique_index(df_teams)
-# assert dval_unique_index(df_players_sum)
+assert dval_unique_index(df_fixtures)
+assert dval_unique_index(df_gameweeks)
+assert dval_unique_index(df_teams)
+assert dval_unique_index(df_positions)
+assert dval_unique_index(df_players_sum)
+assert dval_unique_index(df_players_prev_seasons)
+assert dval_unique_index(df_players_past)
+assert dval_unique_index(df_players_future)
