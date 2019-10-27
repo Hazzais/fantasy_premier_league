9ea432037459881acd92cd89404bf7e93adf8c94
harry-ubuntu-18.04.3 <harryafirth@gmail.com>
2019-10-27 16:39:30 +0000

    Added S3 upload to extract script

--- run_extract.py
+++ run_extract.py
@@ -4,8 +4,11 @@ import argparse
 from fpltools.extract import (retrieve_data, retrieve_player_details,
                               save_intermediate_data)
 from fpltools.constants import API_URLS
-from fpltools.utils import get_datetime
+from fpltools.utils import get_datetime, AwsS3
 
+FILE_STRING_FIXTURES = 'fixtures'
+FILE_STRING_PLAYERS = 'players'
+FILE_STRING_MAIN = 'main'
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Batch download from FPL'
@@ -16,6 +19,20 @@ if __name__ == '__main__':
                         type=str,
                         default='data/',
                         help='path in which to store data')
+    parser.add_argument('-s',
+                        '--skip-s3-upload',
+                        action='store_true',
+                        help='Do not attempt any upload to AWS S3')
+    parser.add_argument('-b',
+                        '--s3_bucket',
+                        type=str,
+                        default='fpl-alldata',
+                        help='S3 bucket to upload to')
+    parser.add_argument('-f',
+                        '--s3-folder',
+                        type=str,
+                        default='etl_staging',
+                        help='Folder within the S3 bucket to upload to')
     args = parser.parse_args()
 
     DATA_LOC = args.data_location
@@ -31,8 +48,16 @@ if __name__ == '__main__':
                                           main_data['elements'],
                                           verbose=True)
 
-    save_intermediate_data(main_data, 'main', DATA_LOC)
-    save_intermediate_data(fixtures_data, 'fixtures', DATA_LOC)
-    save_intermediate_data(player_data, 'players', DATA_LOC)
+    save_intermediate_data(main_data, FILE_STRING_MAIN, DATA_LOC)
+    save_intermediate_data(fixtures_data, FILE_STRING_FIXTURES, DATA_LOC)
+    save_intermediate_data(player_data, FILE_STRING_PLAYERS, DATA_LOC)
+
+    if not args.skip_s3_upload:
+        dfiles = [f'{DATA_LOC}/{FILE_STRING_FIXTURES}.json',
+                  f'{DATA_LOC}/{FILE_STRING_MAIN}.json',
+                  f'{DATA_LOC}/{FILE_STRING_PLAYERS}.json']
+
+        ec2 = AwsS3()
+        ec2.upload(dfiles, args.s3_bucket, args.s3_bucket)
 
     logging.info('================Extract complete================')
