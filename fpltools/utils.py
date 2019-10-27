55c690bf949fc00461356c41dd946a8b9da33d6d
harry-ubuntu-18.04.3 <harryafirth@gmail.com>
2019-10-27 16:39:02 +0000

    Minor change to AWS S3 class to set datetime in init

--- fpltools/utils.py
+++ fpltools/utils.py
@@ -1,8 +1,83 @@
+import os
+import logging
 from datetime import datetime
+
+import boto3
+from botocore.exceptions import ClientError
+
 
 def get_datetime():
     return round(datetime.now().timestamp())
 
+
 def get_datetime_string():
     return str(get_datetime())
 
+
+def get_datetime_string_f(ftime="%Y%m%d-%H%M%S"):
+    return datetime.now().strftime(ftime)
+
+
+class AwsS3:
+    """Upload one or more files to a given S3 bucket"""
+
+    def __init__(self):
+        self.__s3_client = boto3.client('s3')
+        self.__datetime_upload = get_datetime_string_f()
+
+    def _upload_file(self, file, bucket, out_object):
+        """Call to s3 client which uploads the file, naming it as specified to a given bucket """
+        try:
+            resp = self.__s3_client.upload_file(file, bucket, out_object)
+        except FileNotFoundError as e1:
+            logging.error(e1)
+        except ClientError as e2:
+            logging.error(e2)
+        else:
+            logging.info(f'Successfully uploaded {file} as {out_object}')
+            return resp
+
+    @staticmethod
+    def _generate_out_name(filename, bucket_folder, datetime_string):
+        """Generate the output object name for a file. This is the original filename with any S3 folder specified as a
+        prefix, appending the current datetime before the extension"""
+        split_file = filename.split('.')
+
+        # Currently require a file to have an extension - no problem extending this to all files but minor refactoring
+        # would be needed
+        if len(split_file) <= 1:
+            raise RuntimeError(f"File to be uploaded must have an extension")
+        extension = split_file[-1]
+
+        data_out_name = filename.replace(f".{extension}", f"_{datetime_string}.{extension}")
+        if bucket_folder is not None:
+            data_out_object = f'{bucket_folder}/{data_out_name}'
+        else:
+            data_out_object = f'{data_out_name}'
+        return data_out_object
+
+    def upload(self, data_files, bucket, bucket_folder):
+        """Upload one or more files to S3
+
+        datafiles: str or list
+            One or more files (including pre-pending path and extension)
+        bucket: str
+            The S3 bucket to upload to
+        bucket_folder: str or None
+            If specified, the folder(s) within the S3 bucket to upload to
+        """
+        if isinstance(data_files, str):
+            data_files = [data_files]
+        elif isinstance(data_files, list):
+            data_files = data_files
+        else:
+            logging.error("Incorrect set of data file(s) to be uploaded supplied")
+            raise TypeError("data_files must be a str or a list")
+
+        logging.info(f'Uploading {len(data_files)} files to S3 ({bucket})')
+
+        for fl in data_files:
+            logging.info(f'Uploading {fl}')
+            filepath, filename = os.path.split(fl)
+            out_object = self._generate_out_name(filename, bucket_folder, datetime_string=self.__datetime_upload)
+            _ = self._upload_file(fl, bucket, out_object)
