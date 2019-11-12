88c556a10c7022b461bfb4e13016d1931df574bb
harry-ubuntu-18.04.3 <harryafirth@gmail.com>
2019-11-12 19:00:10 +0000

    Made lambda deploy into class

--- lambda_deploy.py
+++ lambda_deploy.py
@@ -5,111 +5,85 @@ import boto3
 
 from fpltools.utils import AwsS3
 
+# TODO: logging
+# TODO: testing (me)
+# TODO: testing (unit tests etc.)
+# TODO: automate lambda update on AWS
+# TODO: move class outside of file?
+# TODO: document
+
 LAMBDA_FUNCTIONS = {'Extract':
-                        {'function_module':
-                             'aws_lambda/aws_lambda_extract.py',
+                        {'function_module':'aws_lambda/aws_lambda_extract.py',
                          'dependencies':
-                             {'internal':
-                                  ['fpltools'],
-                              'external':
-                                  ['requests'],
+                             {'internal': ['fpltools'],
+                              'external': ['requests']
                               },
                          's3':
                              {'bucket': 'fpl-alldata',
-                              'out_object': 'lambda_layers/live_fpl_extract.zip'}}
+                              'out_object': 'lambda_layers/live_fpl_extract.zip'}
+                         }
                     }
-
-BUILD_LOCATION = 'aws_lambda/deploy_builds'
-
-for lf_n, lf_v in LAMBDA_FUNCTIONS.items():
-    print(lf_n)
-    lf_build_loc = os.path.join(BUILD_LOCATION, lf_n)
-    ls_build_deps = os.path.join(lf_build_loc, 'python', 'lib', 'site-packages')
-    os.makedirs(ls_build_deps, exist_ok=True)
-
-    lambda_func = lf_v['function_module']
-    shutil.copy(lambda_func, os.path.join(lf_build_loc, os.path.split(lambda_func)[-1]))
-
-    for dep_internal in lf_v['dependencies']['internal']:
-        # Not explicitly tested for nested dependencies but outside scope of
-        # requirements
-        shutil.copytree(dep_internal, os.path.join(ls_build_deps, dep_internal))
-
-    dep_list_external = " ".join(lf_v['dependencies']['external'])
-    pip_cmd = f"pip install {dep_list_external} -t {ls_build_deps}"
-    os.system(pip_cmd)
-
-    deploy_package = os.path.join(BUILD_LOCATION,
-                                     f'{lf_n}_deploy_package')
-    shutil.make_archive(deploy_package,
-                        'zip', root_dir=lf_build_loc)
-
-    s3_client = boto3.client('s3')
-
-    bucket = lf_v['s3']['bucket']
-    obj = lf_v['s3']['out_object']
-
-    AwsS3()._upload_file(deploy_package + '.zip', bucket, obj)
-
-    shutil.rmtree(BUILD_LOCATION)
-
-
 
 
 class AwsLambdaDeploy:
 
-    def __init__(self,
-                 versioning=False,
-                 lambda_folder='aws_lambda',
-                 deploy_build_root='aws_lambda/deploy_builds'):
-        self.__versioning = versioning
-        self.__lambda_folder = lambda_folder
-        self.__deploy_build_root = deploy_build_root
+    def __init__(self, lambda_name, lambda_data,
+                 build_location='aws_lambda/deploy_builds',
+                 dependencies_subpath='python/lib/site-packages'):
+        self.lf_n = lambda_name
+        self.lf_v = lambda_data
+        self.build_location = build_location
+        self.deps_folder = dependencies_subpath
+        self.lf_build_loc = os.path.join(self.build_location, self.lf_n)
+        self.ls_build_deps = os.path.join(self.lf_build_loc, self.deps_folder)
+        self.lambda_function = self.lf_v['function_module']
+        self.internal_dependencies = lf_v['dependencies']['internal']
+        self.external_dependencies = " ".join(lf_v['dependencies']['external'])
+        self.deploy_package = os.path.join(self.build_location,
+                                           f'{self.lf_n}_deploy_package')
+        self.s3_client = boto3.client('s3')
 
-    def build(self, lambda_function, out_name):
-        """Wrapper to build a zip folder (i.e. the deployment package)"""
-        self._create_build_folder(lambda_function)
-        imports = self._get_imports(lambda_function)
-        requirements = self._get_requirements(imports)
-        self._install_requirements(requirements)
-        self._copy_local()
-        self._zip_build()
-        self._teardown_build_folder()
-        pass
+    def __setup_build_loc(self):
+        os.makedirs(self.ls_build_deps, exist_ok=True)
 
-    def deploy(self, build_folder, bucket, file):
-        pass
+    def __retrieve_lambda_function(self):
+        shutil.copy(self.lambda_function,
+                    os.path.join(self.lf_build_loc,
+                                 os.path.split(self.lambda_function)[-1]))
 
-    def _create_build_folder(self, lambda_function, build_folder=None):
-        if build_folder is None:
-            build_folder = self.__deploy_build_root
+    def __retrieve_internal_dependencies(self):
+        for dep_internal in self.internal_dependencies:
+            # Not explicitly tested for nested dependencies but outside scope of
+            # requirements
+            shutil.copytree(dep_internal, os.path.join(self.ls_build_deps,
+                                                       dep_internal))
 
-        if not os.path.exists(build_folder):
-            raise FileNotFoundError("Build folder does not exist")
+    def __retrieve_external_dependencies(self):
+        pip_cmd = f"pip install {self.external_dependencies} -t {self.ls_build_deps}"
+        os.system(pip_cmd)
 
-        os.mkdir(os.path.join(build_folder, lambda_function.split('.')[0]))
+    def __zip(self):
+        shutil.make_archive(self.deploy_package,
+                            'zip', root_dir=self.lf_build_loc)
 
-    def _get_imports(self, lambda_function):
-        pass
+    def __upload(self):
+        bucket = self.lf_v['s3']['bucket']
+        obj = self.lf_v['s3']['out_object']
+        AwsS3()._upload_file(self.deploy_package + '.zip', bucket, obj)
 
-    def _get_requirements(self, imports):
-        pass
+    def __teardown_build_loc(self):
+        shutil.rmtree(self.build_location)
 
-    def _install_requirements(self, requirements):
-        pass
+    def deploy(self):
+        self.__setup_build_loc()
+        self.__retrieve_lambda_function()
+        self.__retrieve_internal_dependencies()
+        self.__retrieve_external_dependencies()
+        self.__zip()
+        self.__upload()
+        self.__teardown_build_loc()
 
-    def _copy_local(self):
-        pass
-
-    def _zip_build(self):
-        pass
-
-    def _teardown_build_folder(self):
-        pass
-
-# For each lambda function in folder, get imports, create build folder
-# Pip install imports in that folder
-# Zip
-# Teardown build folder
 if __name__ == '__main__':
-    pass
+    for lf_n, lf_v in LAMBDA_FUNCTIONS.items():
+        ld = AwsLambdaDeploy(lf_n, lf_v)
+        ld.deploy()
